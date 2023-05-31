6322b339ac7f5f67fd97f38ab8d8c74042a6ad2e
Sam <samuel.waters@gmail.com>
2023-05-31 10:27:17 -0600

    chore: added css

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { useUser, UserButton } from "@clerk/clerk-react";
+import { useUser, UserButton, useAuth } from "@clerk/clerk-react";
 import styles from '../style/Welcome.module.css';
 
 
 
 function Welcome() {
     const {user, isLoaded} = useUser();
+    const {sessionId} = useAuth();
 
     return (
         <div className={styles.container}>
             <div>
                 <h1>Welcome {user.firstName}</h1>
+                <p>Your session id is {sessionId}</p>
                 <UserButton />
             </div>
         </div>

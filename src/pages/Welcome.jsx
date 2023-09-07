978e1ae62de8242cfe4d8b9d1c2115943e880d3f
Sam Waters <samuel.waters@gmail.com>
2023-09-07 17:43:14 -0600

    chore: updated

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,12 +7,16 @@ import styles from '../style/Welcome.module.css';
 function Welcome() {
     const {user, isLoaded} = useUser();
     const {sessionId} = useAuth();
+    console.log(user)
 
     return (
         <div className={styles.container}>
             <div>
                 <h1>Welcome {user.firstName}</h1>
                 <p>Your session id is {sessionId}</p>
+                <p>Your user id is {user.id}</p>
+                <p>Your full name is {user.fullName}</p>
+
                 <UserButton />
             </div>
         </div>
